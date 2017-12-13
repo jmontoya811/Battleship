e97c319f78d768d41d332f7147789f11206920d6
Alex <alexb3030@gmail.com>
2017-12-13 16:29:21 -0700

    Basic grid 'Templates' plus implementation

---
+++ grid.rb
@@ -0,0 +1,89 @@
+# puts "  A   B   C   D   E    "
+# puts "|---|---|---|---|---|  "
+# puts "| O |   |   | X |   | 1"
+# puts "|---|---|---|---|---|  "
+# puts "|---|---|---|---|---|  "
+# puts "| O |   |   |   |   | 2"
+# puts "|---|---|---|---|---|  "
+# puts "|---|---|---|---|---|  "
+# puts "|   |   | X |   |   | 3"
+# puts "|---|---|---|---|---|  "
+# puts "|---|---|---|---|---|  "
+# puts "|   | X |   | X |   | 4"
+# puts "|---|---|---|---|---|  "
+# puts "|---|---|---|---|---|  "
+# puts "|   | O | O | O |   | 5"
+# puts "|---|---|---|---|---|  "
+
+# lBreaker = "|---|---|---|---|---|  "
+# lined = "|   |   |   |   |   | 1"
+# lArray = lined.split(" ")
+# i = 0
+
+# lArray.each do  
+#   if i == 1
+#     lArray[i] << " O "
+#   else
+#     lArray[i] << "   "
+#   end
+#   i += 1
+#   lArray
+# end
+
+# puts lBreaker
+# puts lArray.join()
+# puts lBreaker
+
+# lined = "|   |   |   |   |   | 4"
+# lArray = lined.split(" ")
+# i = 0
+
+# lArray.each do  
+#   if i == 3
+#     lArray[i] << " O "
+#   else
+#     lArray[i] << "   "
+#   end
+#   i += 1
+#   lArray
+# end
+
+# puts lBreaker
+# puts lArray.join()
+# puts lBreaker
+
+# if row == 2 && col == "A"
+#   if hit
+#     lArray.each do  
+#       if i == 0
+#         lArray[i] << " O "
+#       else
+#         lArray[i] << "   "
+#       end
+#       i += 1
+#       lArray
+#     end
+#   else
+#     lArray.each do  
+#       if i == 0
+#         lArray[i] << " X "
+#       else
+#         lArray[i] << "   "
+#       end
+#       i += 1
+#       lArray
+#     end
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
